eda33262cf495ee3d49ea0879d6de92b6c459302
Velislav Dimitrov <114980635+velislav7@users.noreply.github.com>
2022-12-07 22:42:26 +0200

    ...

--- Project1_PHP/header.php
+++ Project1_PHP/header.php
@@ -14,7 +14,15 @@
    <header class="page-header">
       <div class="header-left">
 
-         
+         <?php
+         if(function_exists('the_custom_logo')){
+
+            $custom_logo_id = get_theme_mod('custom_logo');
+            $logo = wp_get_attachment_image_src($custom_logo_id);
+
+         }
+         ?>
+         <img class="logo" src="<?php echo $logo[0] ?>">
 
          <div class="nav">
             <input type="checkbox" id="nav-check">
@@ -28,21 +36,15 @@
             
             <div class="nav-links">
               <a class="active" href="hex.html">Home</a>
-              <a class="" href="index.html">
-                  <?php echo get_blockinfo('name'); ?>
-              </a>
+              <a class="" href="index.html">Property Management</a>
             </div>
          </div>
 
          <div class="dropdown">
             <button onclick="myFunction()" class="dropbtn">☰</button>
             <div id="myDropdown" class="dropdown-content">
-              <a href="hex.html">
-                  <?php echo get_blockinfo('name'); ?>
-              </a>
-              <a href="index.html">
-                  <?php echo get_blockinfo('name2'); ?>
-              </a>
+              <a href="hex.html">Home</a>
+              <a href="index.html">Property Managment</a>
             </div>
          </div> 
       </div>
