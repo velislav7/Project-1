7aa6ee5c7d7a96df487bbe02cc43346fa99d225a
Velislav Dimitrov <114980635+velislav7@users.noreply.github.com>
2022-12-07 22:51:01 +0200

    dynamic text

--- Project1_PHP/index.php
+++ Project1_PHP/index.php
@@ -6,7 +6,7 @@
       <div class="mySlides fade">
         <img class="bg-image" src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width:100%">
         <div class="text-slideshow">
-           <span class="text-slideshow-sm">LIGHTNING FAST</span>
+           <span class="text-slideshow-sm"><?php echo get_bloginfo('name'); ?></span>
            <span class="text-slideshow-xl">NO-OBLIGATION CASH OFFERS</span>
            <a href="tel:"><button class="btn" type="button">get cash offer now</button></a>
         </div>
