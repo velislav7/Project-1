9db6fdebcbfd545cf1c6e6c428d93a8056095004
Velislav Dimitrov <114980635+velislav7@users.noreply.github.com>
2022-12-11 20:05:28 +0200

    ....

--- Project1_PHP/header.php
+++ Project1_PHP/header.php
@@ -70,7 +70,7 @@
          $image = get_field('feature_image');
          $picture = $image['sizes']['large'];
          ?>
-        <img class="bg-image" src="<?php echo $picture;?>">
+        <img class="bg-image" src="<?php echo $picture;?>" width="100%">
         <div class="text-slideshow">   
            <span class="text-slideshow-sm"><?php echo $hero['small_title_slide1'];?></span>
            <span class="text-slideshow-xl"><?php echo $hero['main_title_slide1'];?></span>
@@ -84,7 +84,7 @@
          $image = get_field('feature_image2');
          $picture = $image['sizes']['large'];
          ?>
-        <img class="bg-image" src="<?php echo $picture;?>">
+        <img class="bg-image" src="<?php echo $picture;?>" width="100%">
         <div class="text-slideshow">
             <span class="text-slideshow-sm"><?php echo $hero['small_title_slide2'];?></span>
             <span class="text-slideshow-xl"><?php echo $hero['main_title_slide2'];?></span>
