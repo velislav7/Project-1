1c14ae8f1cd434a4e82f36c3c5bbaee1dbfd2890
Velislav Dimitrov <114980635+velislav7@users.noreply.github.com>
2022-12-12 18:31:12 +0200

    css

--- Project1.2_PHP/functions.php
+++ Project1.2_PHP/functions.php
@@ -12,8 +12,8 @@ add_action('after_setup_theme', 'anaction_theme_support');
 function anaction_register_styles()
 {
    $version = wp_get_theme()->get( 'Version' );
-    wp_enqueue_style('anaction-slideshow', get_template_directory_uri() . "/assets/css/styles2.css", array(), $version, 'all');
-    wp_enqueue_style('anaction-slideshow', get_template_directory_uri() . "/assets/css/images-grid.css", array(), $version, 'all');
+    wp_enqueue_style('anaction-styles2', get_template_directory_uri() . "/assets/css/styles2.css", array(), $version, 'all');
+    wp_enqueue_style('anaction-grid', get_template_directory_uri() . "/assets/css/images-grid.css", array(), $version, 'all');
     wp_enqueue_style('anaction-styles', get_template_directory_uri() . "/assets/css/styles.css", array(), $version, 'all');
     wp_enqueue_style('anaction-navigation', get_template_directory_uri() . "/assets/css/nav.css", array(), $version, 'all');
    
